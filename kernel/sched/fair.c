b143117faff0b64e5d041ca00fa3df6d72eb8d51
Chris Mason <clm@fb.com>
2025-06-26 07:39:10 -0700

    sched/fair: Bump sd->max_newidle_lb_cost when newidle balance fails
    [ Upstream commit 155213a2aed42c85361bf4f5c817f5cb68951c3b ]
    
    schbench (https://github.com/masoncl/schbench.git) is showing a
    regression from previous production kernels that bisected down to:
    
    sched/fair: Remove sysctl_sched_migration_cost condition (c5b0a7eefc)
    
    The schbench command line was:
    
    schbench -L -m 4 -M auto -t 256 -n 0 -r 0 -s 0
    
    This creates 4 message threads pinned to CPUs 0-3, and 256x4 worker
    threads spread across the rest of the CPUs.  Neither the worker threads
    or the message threads do any work, they just wake each other up and go
    back to sleep as soon as possible.
    
    The end result is the first 4 CPUs are pegged waking up those 1024
    workers, and the rest of the CPUs are constantly banging in and out of
    idle.  If I take a v6.9 Linus kernel and revert that one commit,
    performance goes from 3.4M RPS to 5.4M RPS.
    
    schedstat shows there are ~100x  more new idle balance operations, and
    profiling shows the worker threads are spending ~20% of their CPU time
    on new idle balance.  schedstats also shows that almost all of these new
    idle balance attemps are failing to find busy groups.
    
    The fix used here is to crank up the cost of the newidle balance whenever it
    fails.  Since we don't want sd->max_newidle_lb_cost to grow out of
    control, this also changes update_newidle_cost() to use
    sysctl_sched_migration_cost as the upper limit on max_newidle_lb_cost.
    
    Signed-off-by: Chris Mason <clm@fb.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Acked-by: Vincent Guittot <vincent.guittot@linaro.org>
    Link: https://lkml.kernel.org/r/20250626144017.1510594-2-clm@fb.com
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -12205,8 +12205,14 @@ update_newidle_cost(struct sched_domain *sd, u64 cost, unsigned int success)
 		/*
 		 * Track max cost of a domain to make sure to not delay the
 		 * next wakeup on the CPU.
+		 *
+		 * sched_balance_newidle() bumps the cost whenever newidle
+		 * balance fails, and we don't want things to grow out of
+		 * control.  Use the sysctl_sched_migration_cost as the upper
+		 * limit, plus a litle extra to avoid off by ones.
 		 */
-		sd->max_newidle_lb_cost = cost;
+		sd->max_newidle_lb_cost =
+			min(cost, sysctl_sched_migration_cost + 200);
 		sd->last_decay_max_lb_cost = now;
 
 	} else if (time_after(now, next_decay)) {
@@ -12931,6 +12937,13 @@ static int sched_balance_newidle(struct rq *this_rq, struct rq_flags *rf)
 			domain_cost = t1 - t0;
 			curr_cost += domain_cost;
 			t0 = t1;
+
+			/*
+			 * Failing newidle means it is not effective;
+			 * bump the cost so we end up doing less of it.
+			 */
+			if (!pulled_task)
+				domain_cost = (3 * sd->max_newidle_lb_cost) / 2;
 
 			/*
 			 * Track max cost of a domain to make sure to not delay the
