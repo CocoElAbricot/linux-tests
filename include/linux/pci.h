9aae0d72bbd702687bf6b9722739d28529fbe11f
Chunjie Zhu <chunjie.zhu@cloud.com>
2025-07-18 18:02:35 +0800

    CP-46343: common data structure padding
    Signed-off-by: Chunjie Zhu <chunjie.zhu@cloud.com>

--- include/linux/pci.h
+++ include/linux/pci.h
@@ -529,6 +529,8 @@ struct pci_dev {
 
 	/* These methods index pci_reset_fn_methods[] */
 	u8 reset_methods[PCI_NUM_RESET_METHODS]; /* In priority order */
+
+	void	*xs_kabi_padding;
 };
 
 static inline struct pci_dev *pci_physfn(struct pci_dev *dev)
@@ -678,6 +680,8 @@ struct pci_bus {
 	struct bin_attribute	*legacy_mem;	/* Legacy mem */
 	unsigned int		is_added:1;
 	unsigned int		unsafe_warn:1;	/* warned about RW1C config write */
+
+	void	*xs_kabi_padding;
 };
 
 #define to_pci_bus(n)	container_of(n, struct pci_bus, dev)
@@ -933,6 +937,7 @@ struct pci_driver {
 	struct device_driver	driver;
 	struct pci_dynids	dynids;
 	bool driver_managed_dma;
+	void	*xs_kabi_padding;
 };
 
 static inline struct pci_driver *to_pci_driver(struct device_driver *drv)
