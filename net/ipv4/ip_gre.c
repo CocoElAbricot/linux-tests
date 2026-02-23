5472e9ee528613b681e875ae8da1206c0b360973
john doe <johndoe@example.com>
2026-02-23 13:27:47 +0100

    enable-fragmention-gre-packets

--- net/ipv4/ip_gre.c
+++ net/ipv4/ip_gre.c
@@ -525,8 +525,8 @@ static void gre_fb_xmit(struct sk_buff *skb, struct net_device *dev,
 
 	gre_build_header(skb, tunnel_hlen, flags, proto,
 			 tunnel_id_to_key32(tun_info->key.tun_id),
-			 test_bit(IP_TUNNEL_SEQ_BIT, flags) ?
-			 htonl(atomic_fetch_inc(&tunnel->o_seqno)) : 0);
+			 test_bit(IP_TUNNEL_SEQ_BIT, flags) ? htonl(atomic_fetch_inc(&tunnel->o_seqno)) : 0);
+	skb->ignore_df = 1;
 
 	ip_md_tunnel_xmit(skb, dev, IPPROTO_GRE, tunnel_hlen);
 
