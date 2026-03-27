xcp-ng/linux

pci: export pci_probe_reset_function()
The xen-pciback needs to test if a per-function reset is available so
it can provide an interface for a safe bus reset instead.

Signed-off-by: David Vrabel <david.vrabel@citrix.com>

drivers/pci/pci.c
@@ -4768,6 +4768,7 @@ int pci_probe_reset_function(struct pci_dev *dev)
 
 	return pci_parent_bus_reset(dev, 1);
 }
+EXPORT_SYMBOL_GPL(pci_probe_reset_function);
 
 /**
  * pci_reset_function - quiesce and reset a PCI device function

CocoElAbricot/linux-tests

drivers/pci/pci.c
@@ -95,6 +95,7 @@ bool pci_reset_supported(struct pci_dev *dev)
 {
 	return dev->reset_methods[0] != 0;
 }
+EXPORT_SYMBOL_GPL(pci_reset_supported);
 
 #ifdef CONFIG_PCI_DOMAINS
 int pci_domains_supported = 1;